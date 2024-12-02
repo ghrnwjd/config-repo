b048930dc9265bf41355ebe478b0ee5671347e77
정호영 <ghrnwjd@MacBookPro.local>
2024-12-02 15:45:41 +0900

    feat: administrative process with actuator test

---
+++ config-service/src/test/java/com/hoyoung/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoyoung.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
